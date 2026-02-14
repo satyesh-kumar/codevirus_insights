4d101abdb1c8603c3037f4b3dde57142df30263d
satyesh kumar <satyeshkumar578@gmail.com>
2026-02-14 21:41:35 +0530

    update backend

--- backend/server.js
+++ backend/server.js
@@ -11,12 +11,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
 mongoose
     .connect(process.env.MONGO_URL)
     .then(() => console.log("MongoDB Connected"))
     .catch((err) => console.log(err));
 
 app.use("/api/auth", authRoutes);
+
+app.get("/", (req, res) => {
+    res.send("Welcome to CodeVirus Insights API");
+});
 
 app.listen(process.env.PORT, () =>
     console.log(`Server running on port ${process.env.PORT}`)
