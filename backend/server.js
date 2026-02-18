0a2f24ba0d8f3d954e421971115a6b8f7efa7a7a
satyesh kumar <satyeshkumar578@gmail.com>
2026-02-18 21:28:03 +0530

    add the appcors

--- backend/server.js
+++ backend/server.js
@@ -9,8 +9,10 @@ import authRoutes from "./routes/authRoutes.js";
 const app = express();
 
 app.use(cors({
-    origin: "https://codevirus-insights.vercel.app"
+    origin: ["https://codevirus-insights.vercel.app", "http://localhost:5173"],
+    credentials: true
 }));
+
 app.use(express.json());
 
 
