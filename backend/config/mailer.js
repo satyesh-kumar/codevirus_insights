bed68a48dc64592624afcdc8fdf7f0d1e4067bd5
satyesh kumar <satyeshkumar578@gmail.com>
2026-02-19 14:07:21 +0530

    mailer

--- backend/config/mailer.js
+++ backend/config/mailer.js
@@ -6,13 +6,13 @@ dotenv.config();
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
-  secure: false,
+  secure: false, // Must be false for 587
+  pool: true,    // Helps with timeouts
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
   tls: {
-
     rejectUnauthorized: false,
     minVersion: "TLSv1.2"
   },
