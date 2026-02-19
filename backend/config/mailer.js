ce242cd618aed05b8e5b431d2a39c041a7080fe9
satyesh kumar <satyeshkumar578@gmail.com>
2026-02-19 13:41:38 +0530

    transporter

--- backend/config/mailer.js
+++ backend/config/mailer.js
@@ -1,14 +1,20 @@
-import nodemailer from "nodemailer"; 
+import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  tls: {
+
+    rejectUnauthorized: false
+  }
 });
 
 export const sendOtpEmail = async (email, otp) => {
